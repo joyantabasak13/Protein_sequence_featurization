8ea3e62c957563ee0b895240bfec2061f62d73ac
joyantabasak13 <joyantabasak13@gmail.com>
2021-05-03 02:53:44 +0600

    [Title] EBGW: character sequencing done
    [FeatureName] n/a
    [Modification] n/a
    [Comment] Normalised weight calculation from progressively increasing subsequence remains to-do
    [Researcher] Joyanta
    
    Signed-off-by: joyantabasak13 <joyantabasak13@gmail.com>

--- EBGW/EBGW.py
+++ EBGW/EBGW.py
@@ -1,1 +1,39 @@
-print("Hello, World!")
+import pandas as pd
+
+df = pd.read_csv (r'../Data/miniDataset_window_15 .csv')
+sequence = pd.DataFrame(df, columns= ['Sequence']).values.tolist()
+
+#Sets
+c1 = {'G','A','V','L','I','M','P','F','W'}
+c2 = {'Q','N','S','T','Y','C'}
+c3 = {'D','E'}
+c4 = {'H','K','R'}
+#Charecteristics Sequences
+hSeqList1 = list()
+hSeqList2 = list()
+hSeqList3 = list()
+count =0
+for seq in sequence:
+    hSeq1 = list()
+    hSeq2 = list()
+    hSeq3 = list()
+    #extra "[" and "'" are added in str(seq)
+    for ch in str(seq)[2:-2]:
+        if ch in c1 or ch in c2:
+            hSeq1.append(1)
+        else:
+            hSeq1.append(0)
+        if ch in c1 or ch in c3:
+            hSeq2.append(1)
+        else:
+            hSeq2.append(0)
+        if ch in c1 or ch in c3:
+            hSeq3.append(1)
+        else:
+            hSeq3.append(0)
+    hSeqList1.append(hSeq1)
+    hSeqList2.append(hSeq2)
+    hSeqList3.append(hSeq3)
+#
+
+
